556197a5fee986a74d03144147ffe50470d190f2
Daniel Klotz <dklotz@goc.realcore-it.de>
2019-12-25 11:37:50 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: 'People counter',
+      home: Home()));
+}
+
+class Home extends StatefulWidget {
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int _people = 0;
+  String _infoText = "Come in";
+
+  void _changePeople(int delta) {
+    setState(() {
+      _people += delta;
+    });
+
+    if(_people < 0) {
+      _infoText = "You entered an alternate dimension!";
+    } else if (_people <= 10) {
+      _infoText = "Come in";
+    } else {
+      _infoText = "Crowded!";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.fill,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("People $_people",
+                style: TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text("+1",
+                          style:
+                          TextStyle(fontSize: 40, color: Colors.white)),
+                      onPressed: () {
+                        _changePeople(1);
+                      },
+                    )),
+                Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text("-1",
+                          style:
+                          TextStyle(fontSize: 40, color: Colors.white)),
+                      onPressed: () {
+                        _changePeople(-1);
+                      },
+                    )),
+              ],
+            ),
+            Text(_infoText,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30,)),
+          ],
+        )
+      ],
+    );
+  }
+}
+
